92f23b94c7ee5d465b08b495394ba7b7de787682
Hilmy Naufal <hilmyblaze@gmail.com>
2023-07-10 20:05:52 +0700

    third

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "products"
+    database: "products",
+    port: "3306"
 })
 
 const port = 3000
