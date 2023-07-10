68df98ded1ac8b98af73b01c52957d44b9e0d269
Hilmy Naufal <hilmyblaze@gmail.com>
2023-07-10 19:45:48 +0700

    init

---
+++ index.js
@@ -0,0 +1,54 @@
+// import mysql from 'mysql'
+const mysql = require('mysql')
+const express = require('express')
+const { env } = require('process')
+const bodyParser = require('body-parser')
+const app = express()
+
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "products"
+})
+
+const port = 3000
+
+app.use(bodyParser.json())
+
+app.get('/handphone', (req, res) => {
+    const q = "SELECT * FROM handphone"
+    db.query(q, (err, data) => {
+        if (err) {
+            return res.json(err)
+        }
+        return res.json({data: data})
+    })
+    // res.send('Hello World')
+})
+
+app.post('/handphone', (req, res) => {
+    console.log(req.body)
+    const q = "INSERT INTO handphone (`name`, `brand`) VALUES (?)"
+    const values = [
+        req.body.name,
+        req.body.brand
+    ]
+
+    db.query(q, [values], (e, data) => {
+        if (e) return res.json(e)
+        return res.json("ok")
+    })
+    // res.status(200).send("ok")
+})
+
+
+app.post('/login', (req, res) => {
+    console.log(req.body)
+    res.send("login berhasil")
+})
+
+app.listen(port, () => {
+    console.log(`tai on port = ${port}`);
+})
